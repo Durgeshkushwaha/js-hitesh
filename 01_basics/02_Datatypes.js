ed0fc1358ef8c2f21ed344d032e3323b34f8abee
durgeshkushwaha <durgeshkushwaha361000@gmail.com>
2023-10-31 23:21:42 +0530

    first commit

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version 
+
+// alert(3+3) // we arae using nodejs, not browser
+
+let name="durgesh"
+let isLosggedIn=false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+//boolean =>true/false
+//null => standalone value
+// undefined =>
+// symbil => unique
+
+//object
+console.log(typeof "durgesh");
+console.log(typeof null);  // object
+console.log(typeof undefined); //  undefined
+
